b6390965a6c1fb74b36e52195bfa1856e467beeb
CrazyGamerGR <frankalexb97@gmail.com>
2017-08-23 20:21:33 +0300

    add build_anykernel.sh

---
+++ build_anykernel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright - CrazyGamerGR
+#     		 ________     __   ___
+#    		/ ______|    |  | /  |
+#    		|  |         |  |/  /
+#   		|  |_____    |      \              
+#   		\_______|    |__|\___|
+##########################################
+export ARCH=arm64
+export CROSS_COMPILE=/home/francesco/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+##########################################
+mkdir output
+##########################################
+make -C $(pwd) O=output gemini_defconfig
+make -j64 -C $(pwd) O=output
+##########################################
+cp output/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
