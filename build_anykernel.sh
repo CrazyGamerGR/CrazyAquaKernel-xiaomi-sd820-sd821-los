c94c2e724f4a1581ea69ca4892bc493fc3e1c4fc
CrazyGamerGR <frankalexb97@gmail.com>
2017-08-24 22:41:47 +0300

    update build_anykernel.sh

--- build_anykernel.sh
+++ build_anykernel.sh
@@ -10,9 +10,24 @@
 export ARCH=arm64
 export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 ##########################################
-mkdir output
+mkdir outputgemini
+mkdir outputcapricorn
+mkdir outputnatrium
+mkdir outputlithium
 ##########################################
-make -C $(pwd) O=output gemini_defconfig
-make -j64 -C $(pwd) O=output
+make -C $(pwd) O=outputgemini gemini_defconfig
+make -j64 -C $(pwd) O=outputgemini
+
+make -C $(pwd) O=outputcapricorn capricorn_defconfig
+make -j64 -C $(pwd) O=outputcapricorn
+
+make -C $(pwd) O=outputnatrium natrium_defconfig
+make -j64 -C $(pwd) O=outputnatrium
+
+make -C $(pwd) O=outputlithium lithium_defconfig
+make -j64 -C $(pwd) O=outputlithium
 ##########################################
-cp output/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
+cp outputgemini/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
+cp outputcapricorn/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
+cp outputnatrium/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
+cp outputlithium/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
